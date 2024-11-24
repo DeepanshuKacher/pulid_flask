6792976c0a943c26d7806edd0f59f4eef1ee770c
Deepanshu <kacher.deepanshu895@gmail.com>
2024-11-24 17:38:54 +0530

    aa

--- app_flask.py
+++ app_flask.py
@@ -114,6 +114,8 @@ def run(*args):
         prompt, (1, H, W), neg_prompt, id_embedding, uncond_id_embedding, id_scale, scale, steps, seed
     )[0]
 
+    print(img)
+
     print('pass 4')
 
     # Debugging the img type and shape
