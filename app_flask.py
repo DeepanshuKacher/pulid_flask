af7de8874cce78b51a00d2a15a9294e6b763be73
Deepanshu <kacher.deepanshu895@gmail.com>
2024-11-24 20:58:01 +0530

    remove all prints

--- app_flask.py
+++ app_flask.py
@@ -130,25 +130,19 @@ def get_image_type(image) -> str:
 @torch.inference_mode()
 def run(*args):
     id_image = args[0]
-    # print('pass1')
 
     # image = Image.open("raj_closeup.jpeg")
 
     # Convert image to NumPy array
     # id_image = np.array(image)
 
-    print('pass after 1 and before 2')
 
     supp_images = args[1:4]
-    print('pass supp_images')
-    print("printing args:- ",args[4:])
     prompt, neg_prompt, scale, seed, steps, H, W, id_scale, num_zero, ortho = args[4:]
-    print('pass prompt and all')
     seed = int(seed)
     if seed == -1:
         seed = torch.Generator(device="cpu").seed()
 
-    print('seed generated')
 
     pipeline.debug_img_list = []
 
@@ -163,7 +157,6 @@ def run(*args):
         attention.ORTHO = False
         attention.ORTHO_v2 = False
 
-    print('pass 2')
 
     if id_image is not None:
         id_image = resize_numpy_image_long(id_image, 1024)
@@ -172,12 +165,10 @@ def run(*args):
         ]
         id_image_list = [id_image] + supp_id_image_list
         uncond_id_embedding, id_embedding = pipeline.get_id_embedding(id_image_list)
-        print('pass 2. last')
     else:
         uncond_id_embedding = None
         id_embedding = None
     
-    print('pass 3')
 
     img = pipeline.inference(
         prompt, (1, H, W), neg_prompt, id_embedding, uncond_id_embedding, id_scale, scale, steps, seed
@@ -186,27 +177,6 @@ def run(*args):
     np_image  = np.array(img)
 
     base64_image = numpy_to_base64(np_image)
-
-    print(get_image_type(base64_image))
-
-    print(img)
-    # print(np_image)
-    # print(base64_image)
-
-
-    print('pass 4')
-
-    # Debugging the img type and shape
-    print(f'img type: {type(img)}')
-
-    # Convert the img (PIL Image) to Base64 if it's a PIL Image
-    if isinstance(img, Image.Image):
-        print("this if is a PIL Image")
-        img_base64 = image_to_base64(img) # Convert PIL Image to NumPy array and then to Base64
-    else:
-        print('this is not a pil image')
-        # If img is already a NumPy array, just convert to Base64
-        img_base64 = numpy_to_base64(img)
 
     return base64_image, str(seed)
 
@@ -262,10 +232,8 @@ def generate():
             ortho,
         ]
 
-        print('print inputs:- ',inps)
 
         output, seed_output  = run(*inps)
-        print('just have to send output')
         return jsonify({
             "output": output,  # Base64-encoded image
             "seed_output": seed_output,
